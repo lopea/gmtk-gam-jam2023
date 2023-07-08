436674994829c400defe0b305c65f7dbc7c90972
Parker Horne <parker.horne@digipen.edu>
2023-07-08 14:03:43 -0700

    food spawner and eating mostly done

--- Assets/Scripts/FoodSystem.cs
+++ Assets/Scripts/FoodSystem.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
 public class FoodSystem : MonoBehaviour
 {
@@ -13,7 +16,7 @@ public class FoodSystem : MonoBehaviour
     // please keep this 100 lol
     [SerializeField] private float _maxFood = 100f;
 
-    [SerializeField] private float _eatAmount = 15f;
+    [SerializeField] private float _eatAmount = 15;
 
     private RectTransform _foodBar;
 
@@ -22,12 +25,14 @@ public class FoodSystem : MonoBehaviour
     private float _timer;
     
     private float _destination;
+    private BaitSpawner _spawner;
     void Start()
     {
         _foodBar = GameObject.Find("FoodMeter").GetComponent<RectTransform>();
+        _spawner = GameObject.Find("BaitSpawner").GetComponent<BaitSpawner>();
         _currFood = _maxFood;
         _timer = 0f;
-        _drain = 10f;
+        _drain = 5f;
     }
     
     void Update()
@@ -45,5 +50,32 @@ public class FoodSystem : MonoBehaviour
         _foodBar.localPosition = new Vector3(Mathf.Lerp(_foodBar.localPosition.x, _destination, _timer),
             _foodBar.localPosition.y, _foodBar.localPosition.z);
         // Vector3 offset = new Vector3(_drain, 0, 0);
+    }
+
+    void EatFood(GameObject food)
+    {
+        int baitRoll = Random.Range(0, 100);
+        if (baitRoll <= percentChanceBait)
+        {
+            // event for bait here
+        }
+        else
+        {
+            Destroy(food);
+            _currFood += _eatAmount;
+            Vector3 offset = new Vector3(_eatAmount, 0, 0);
+            _foodBar.localPosition += offset;
+            _timer = 0f;
+            _destination = _foodBar.localPosition.x - _drain;
+            _spawner.HandleEat(food);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            EatFood(collision.gameObject);
+        }
     }
 }
