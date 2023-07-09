3a8df54ecda926be0146ad4c79eadf1fbe04b47b
lopea <lopea5643@gmail.com>
2023-07-08 20:15:29 -0700

    added bar

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -2,20 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class ChangeColor : MonoBehaviour
+public class ChangeColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private TMP_Text _text;
+
+    [SerializeField] private Color idle, hovered, pressed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _text = GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void ChangeToScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.color = hovered;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.color = idle;
     }
 }
