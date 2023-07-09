080a25f84f9ed0afc7646a5fc9aef92ded21ac4f
lopea <lopea5643@gmail.com>
2023-07-08 18:38:57 -0700

    whoever threw that your moms a hoe

--- Assets/Scripts/Bob.cs
+++ Assets/Scripts/Bob.cs
@@ -1,20 +1,22 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class Bob : MonoBehaviour
 {
     public float freq = 10, amplitude = 1;
     public Vector3 direction = Vector3.up;
-    public Vector3 pos;
-
+    Vector3 pos;
+    private float RandomOffset;
     private void Awake()
     {
+        RandomOffset = Random.Range(0.0f, 2.0f* Mathf.PI);
         pos = transform.position;
     }
 
     private void Update()
     {
-        transform.position = pos + direction * (Mathf.Sin(2.0f * Mathf.PI * freq  * Time.time) * amplitude);
+        transform.position = pos + direction * (Mathf.Sin(2.0f * Mathf.PI * freq  * Time.time) * amplitude + RandomOffset);
     }
 }
